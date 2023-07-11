7b0d375becc264adb5a81014ac0bb65f24331480
deniselupe <dgrodriguez24@hotmail.com>
2023-07-11 19:14:51 +0000

    lesson: your first test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// This checks if the learn react link is present in <App /> or not
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// Now how to run this test?
